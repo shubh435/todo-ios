3679becdc6ebd3f7f8fd7cd0ff084e880f08cee5
shubham sarode <shubham.sarode@extwebtech.in>
2023-01-15 02:57:26 +0530

    added some new fonts and vecot icon

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,56 +1,115 @@
 import React from 'react';
-import {Dimensions, ImageBackground, Text, View} from 'react-native';
+
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 interface TodosProps {}
 
-interface TodosState {}
+interface Todo {
+  name: string;
+  id: string;
+  status: 'Pending' | 'Completed' | 'Waiting';
+}
+interface TodosState {
+  name: string;
+  todos: Todo[];
+}
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 class Todos extends React.Component<TodosProps, TodosState> {
   constructor(props: TodosProps) {
     super(props);
-    this.state = {};
+    this.state = {name: '', todos: []};
   }
   render() {
     return (
       <>
-        {/* <Text>App</Text> */}
-
         <ImageBackground
           source={require('../assets/background.png')}
-          style={{
-            width: width,
-            height: height / 3,
-            position: 'relative',
-          }}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'flex-end',
-              alignItems: 'flex-end',
-              paddingBottom: 5,
-              paddingRight: 5,
-            }}>
-            <Text
-              style={{
-                color: '#fff',
-                fontSize: 18,
-                fontWeight: '400',
-                fontFamily: 'Russo One',
-              }}>
-              Thu 9
-            </Text>
-            <Text
-              style={{
-                color: '#fff',
-                fontSize: 30,
-              }}>
-              6:23 AM
-            </Text>
+          style={styles.imageBackground}>
+          <View style={styles.backgroudView}>
+            <Text style={[styles.text, styles.smallText]}>Thu 9</Text>
+            <Text style={[styles.text, styles.bigText]}>6:23 AM</Text>
           </View>
         </ImageBackground>
+
+        <View style={styles.viewTextInput}>
+          <TextInput
+            style={styles.textInput}
+            value={this.state.name}
+            placeholder="Note"
+            onChangeText={(text: string) => this.setState({name: text})}
+          />
+          <TouchableOpacity style={styles.btnGreen}>
+            <Icon color="#fff" size={30} name="plus" />
+          </TouchableOpacity>
+        </View>
       </>
     );
   }
 }
+const styles = StyleSheet.create({
+  imageBackground: {
+    width: width,
+    height: height / 3,
+    position: 'relative',
+  },
+  backgroudView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingBottom: 10,
+    paddingRight: 30,
+  },
+  text: {
+    fontWeight: '400',
+    color: '#fff',
+    fontFamily: 'Russo One',
+  },
+  smallText: {
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  bigText: {
+    lineHeight: 58,
 
+    fontSize: 48,
+  },
+  textInput: {
+    backgroundColor: '#EBEFF2',
+    flex: 1,
+    height: height / 18,
+    color: '#888888',
+
+    borderRadius: 3,
+    marginRight: 10,
+    textTransform: 'capitalize',
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 16,
+    lineHeight: 18,
+  },
+  viewTextInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: width,
+    marginTop: 20,
+    paddingLeft: 29,
+    paddingRight: 28,
+    alignItems: 'center',
+  },
+  btnGreen: {
+    backgroundColor: '#20EEB0',
+    padding: 10,
+    borderRadius: 3,
+  },
+});
 export default Todos;
